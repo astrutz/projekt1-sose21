eeaaeea5e85143fb46b49b595abadb556cca2c5f
astrutz <--replace-all>
2021-07-19 17:42:48 +0200

    Initiate backend project

---
+++ prototype/backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Test');
+})
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+module.exports = app;
