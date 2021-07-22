25b5a48ced6762b2492d0c85ec994c10ca1d4a65
astrutz <--replace-all>
2021-07-22 13:53:06 +0200

    Add logging

--- prototype/backend/app.js
+++ prototype/backend/app.js
@@ -4,15 +4,11 @@ const app = express();
 const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 
-// just to test the server
-app.get('/', (req, res) => {
-  res.status(200).send('Working');
-})
-
 //Socket Logic
 const socketio = require('socket.io')(server)
 
 socketio.on("connection", (userSocket) => {
+    console.log('socket connection established')
     // Listen to message event
     userSocket.on("send_message", (data) => {
         console.log('Received message', data);
