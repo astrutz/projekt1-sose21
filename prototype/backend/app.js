00033f1c69b8a461d7add1393fa0d2bb74166cb6
astrutz <--replace-all>
2021-07-22 17:07:30 +0200

    Set history serverside

--- prototype/backend/app.js
+++ prototype/backend/app.js
@@ -1,22 +1,28 @@
 const express = require('express');
+const history = require('./history');
 
 const app = express();
 const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 
 //Socket Logic
-const socketio = require('socket.io')(server)
+const socketio = require('socket.io')(server);
 
 socketio.on("connection", (userSocket) => {
-    console.log('socket connection established')
+
+    console.log('Socket connection established', userSocket)
+
     // Listen to message event
     userSocket.on("send_message", (data) => {
-        console.log('Received message', data);
-        // Send message to all sockets
-        userSocket.broadcast.emit("receive_message", data);
-    })
-})
+
+      history.setHistory(data);
+      
+      console.log('Received message', data);
+      // Send message to all sockets
+      userSocket.broadcast.emit("receive_message", data);
+    });
+});
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
-})
+});
---
+++ prototype/backend/history.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+async function setHistory(messageObject) {
+    // if history
+    const directory = fs.readdirSync('');
+    console.log('DIR', directory);
+    let history = { messages: [] };
+    if(directory.includes('history.json')) {
+        history = JSON.parse(fs.readFileSync('history.json'));
+    }
+    history.messages.push(messageObject);
+    fs.writeFileSync('history.json', JSON.stringify(history));
+}
+
+async function getHistory() {
+    
+}
+
+module.exports = {
+    setHistory,
+    getHistory
+};
