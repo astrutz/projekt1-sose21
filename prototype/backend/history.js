9358df20233e6a76c628e0c6c9e43538f4798129
astrutz <--replace-all>
2021-07-22 17:13:45 +0200

    Try dirname

--- prototype/backend/history.js
+++ prototype/backend/history.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
+const path = require('path');
 
 async function setHistory(messageObject) {
     // if history
-    const directory = fs.readdirSync('/');
+    const directory = fs.readdirSync(__dirname);
     console.log('DIR', directory);
     let history = { messages: [] };
     if(directory.includes('history.json')) {
-        history = JSON.parse(fs.readFileSync('history.json'));
+        history = JSON.parse(fs.readFileSync(path.join(__dirname, 'history.json')));
     }
     history.messages.push(messageObject);
-    fs.writeFileSync('history.json', JSON.stringify(history));
+    fs.writeFileSync(path.join(__dirname, 'history.json'), JSON.stringify(history));
 }
 
 async function getHistory() {
