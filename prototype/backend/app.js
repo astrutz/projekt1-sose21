8b09905a520e6cd3b12a9dfa4bfb5f02b28ee738
astrutz <--replace-all>
2021-07-22 13:29:05 +0200

    Adding socket.io to server

--- prototype/backend/app.js
+++ prototype/backend/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 
 const app = express();
+const server = require('http').createServer(app);
+const port = process.env.PORT || 3000;
 
+// just to test the server
 app.get('/', (req, res) => {
-    res.send('Test');
+  res.status(200).send('Working');
 })
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+//Socket Logic
+const socketio = require('socket.io')(server)
 
-module.exports = app;
+socketio.on("connection", (userSocket) => {
+    // Listen to message event
+    userSocket.on("send_message", (data) => {
+        console.log('Received message', data);
+        // Send message to all sockets
+        userSocket.broadcast.emit("receive_message", data);
+    })
+})
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+})
